e84fee13fa3d23599434a2a5fffd2ba46b58dbeb
Purnima Kesarwani <purnimakesarwani47@gmail.com>
2024-12-08 23:11:45 +0530

    REACT JS Basics

--- ES6 Concepts/ternaryOperator.js
+++ ES6 Concepts/ternaryOperator.js
@@ -1,1 +1,21 @@
 console.log("TERNARY OPERATORS");
+
+let showRecipeOne = true;
+
+function getRecipeOneName(recipeName) {
+    return recipeName;
+}
+
+function getRecipeTwoName(recipeName) {
+    return recipeName;
+}
+
+// if (showRecipeOne) {
+//     console.log(getRecipeOneName("Burger"));
+// } else {
+//     console.log(getRecipeTwoName("Coke"));
+// }
+
+// condition ? statement1 : statement2;
+
+showRecipeOne ? console.log(getRecipeOneName("Burger")) : console.log(getRecipeTwoName("Coke"));
