da999981043f4d31577935ec4cfc9c2ff73a7fc0
Purnima Kesarwani <purnimakesarwani47@gmail.com>
2024-12-09 10:16:09 +0530

    REACT JS Basics

---
+++ react-js-concept/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import ClassBasedComponent from './components/class-based-component'
+
+function App() {
+  return (
+    <div>
+      <h1>REACT JS CONCEPTS</h1>
+      <ClassBasedComponent />
+    </div>
+  )
+}
+
+export default App
